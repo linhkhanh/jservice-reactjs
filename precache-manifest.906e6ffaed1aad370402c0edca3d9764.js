6c3e65b1b27f6510e85abdaf51ab903832130a9e
linhle <linhle@Hieus-MacBook-Air.local>
2020-07-22 13:33:41 +0800

    Updates

---
+++ precache-manifest.906e6ffaed1aad370402c0edca3d9764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613c0e880801b25c722e0127abeb4652",
+    "url": "/jservice-reactjs/index.html"
+  },
+  {
+    "revision": "2a08b8f1cc6236d6d732",
+    "url": "/jservice-reactjs/static/css/main.d999cfd0.chunk.css"
+  },
+  {
+    "revision": "b33f3b9192a2ea2a57dd",
+    "url": "/jservice-reactjs/static/js/2.155700d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jservice-reactjs/static/js/2.155700d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a08b8f1cc6236d6d732",
+    "url": "/jservice-reactjs/static/js/main.2365b7ee.chunk.js"
+  },
+  {
+    "revision": "35e2b4228fb7825a8ae2",
+    "url": "/jservice-reactjs/static/js/runtime-main.97b79cf9.js"
+  }
+]);
